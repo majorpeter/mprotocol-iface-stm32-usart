8d2d945eb5aa9ba6aa13efaa20fb6d6ab896c824
Major Péter <majorpeter29@gmail.com>
2018-07-03 19:42:47 +0200

    changed interrupt priority to maximal

--- UsartSerialInterface.cpp
+++ UsartSerialInterface.cpp
@@ -37,7 +37,7 @@ UsartSerialInterface::UsartSerialInterface(uint32_t baudRate) {
 
     NVIC_InitTypeDef NVIC_InitStructure;
     NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
